e2e4e6b6c5eb23954cd3faaacb2fd65babe9c7ed
jscervantes <jscervantes00@gmail.com>
2026-04-01 19:53:09 -0400

    set SHPE logo up.

--- shpe-osu/src/routes/+page.svelte
+++ shpe-osu/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <div class="content">
   <div class="hero">
+    <span class="est">est. 2010</span>
     <h1>SHPE</h1>
     <p>@oregonstate</p>
   </div>
@@ -14,11 +15,19 @@
 </div>
 
 <style>
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
   .hero {
     display: flex;
     flex-direction: column;
     align-items: left;
     padding: var(--size-100);
+  }
+  .est {
+    margin-bottom: -1.5rem
   }
   .hero h1 {
     font-size: var(--size-900);
@@ -26,25 +35,21 @@
     color: var(--color-brand-dark);
   }
   .hero p {
-    font-size: var(--size-400);
+    font-size: var(--size-200);
     font-weight: 400;
     color: var(--color-brand-dark);
-    margin-top: -1.75rem;
+    margin-top: -1.5rem;
   }
   .description h1 {
     font-size: var(--size-300);
     font-weight: 400;
     color: var(--color-brand-dark);
+    padding-left: var(--)
   }
   p {
     font-size: var(--size-100);
     font-weight: 100;
     color: var(--color-brand-dark);
-  }
-  .content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
   }
 
 </style>
